bfa0f507ee63c573f7c44e52e82e5423d09e846d
GHOST <adarshkakkat7@gmail.com>
2021-07-13 20:58:04 +0530

    initial commit implemented flutter firebase notification

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  runApp(MyApp());
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notification Test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return LayoutBuilder(builder: (context,size){
+      return Scaffold(
+          appBar: AppBar(
+            title: Text("Notification Test"),
+          ),
+          body: Container(
+            height: size.maxHeight,
+            width: size.maxWidth,
+            child: Center(child: Text("Notification test",style: TextStyle(fontSize: 30),),),
+          )
+      );
+    });
+  }
+}
